299cc9e4c8de32a162237b8a1ab1903520f986b7
Ashwin Easo Zachariah <ashwineaso@gmail.com>
2025-09-08 20:21:28 +0200

    modifies the main function to run the printing in a loop

--- cmd/mintop/main.go
+++ cmd/mintop/main.go
@@ -2,16 +2,53 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"strconv"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/shirou/gopsutil/v4/cpu"
 )
 
 func main() {
-	fmt.Println("CPU Percentage		: ", getCPUPercentage())
-	fmt.Println("Memory Percentage	: ", getMemoryPercentage())
-	fmt.Println("Disk Percentage	: ", getDiskPercentage())
-	fmt.Println("Running Processes	: ", getRunningProcesses())
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	printChan := make(chan struct{})
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		printSysInfo(printChan)
+	}()
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			printChan <- struct{}{}
+		case <-stopChan:
+			close(printChan)
+			wg.Wait()
+			fmt.Println("Exiting...")
+			return
+		}
+	}
+}
+
+func printSysInfo(printChan chan struct{}) {
+	for range printChan {
+		fmt.Println("CPU Percentage		: ", getCPUPercentage())
+		fmt.Println("Memory Percentage	: ", getMemoryPercentage())
+		fmt.Println("Disk Percentage	: ", getDiskPercentage())
+		fmt.Println("Running Processes	: ", getRunningProcesses())
+	}
 }
 
 func getCPUPercentage() string {
