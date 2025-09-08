03d8d20340dcac35947f1cab227c420fad3852d3
Ashwin Easo Zachariah <ashwineaso@gmail.com>
2025-09-08 20:13:21 +0200

    adds basic version of sys info

--- cmd/mintop/main.go
+++ cmd/mintop/main.go
@@ -1,10 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+)
 
 func main() {
-	fmt.Println("CPU Percentage		: ", "TODO")
-	fmt.Println("Memory Percentage	: ", "TODO")
-	fmt.Println("Disk Percentage	: ", "TODO")
-	fmt.Println("Running Processes	: ", "TODO")
+	fmt.Println("CPU Percentage		: ", getCPUPercentage())
+	fmt.Println("Memory Percentage	: ", getMemoryPercentage())
+	fmt.Println("Disk Percentage	: ", getDiskPercentage())
+	fmt.Println("Running Processes	: ", getRunningProcesses())
+}
+
+func getCPUPercentage() string {
+	cpuTimes, err := cpu.Times(false)
+	if err != nil || len(cpuTimes) == 0 {
+		return "Error fetching CPU times"
+	}
+
+	c := cpuTimes[0]
+	user := strconv.FormatFloat(c.User, 'f', 1, 64)
+	system := strconv.FormatFloat(c.System, 'f', 1, 64)
+	idle := strconv.FormatFloat(c.Idle, 'f', 1, 64)
+	return fmt.Sprintf("User: %s, System: %s, Idle: %s", user, system, idle)
+}
+
+func getMemoryPercentage() float64 {
+	return 0.0
+}
+
+func getDiskPercentage() float64 {
+	return 0.0
+}
+
+func getRunningProcesses() int {
+	return 0
 }
