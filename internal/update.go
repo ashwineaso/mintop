cc3f5d7a820831fb9e52e85a80f3488a80864d26
Ashwin Easo Zachariah <ashwineaso@gmail.com>
2025-09-14 22:55:34 +0200

    adds command to move the highlight up and down

--- internal/update.go
+++ internal/update.go
@@ -21,11 +21,20 @@ func (m Model) Update(teaMsg tea.Msg) (tea.Model, tea.Cmd) {
 		m.width = msg.Width
 		m.height = msg.Height
 		return m, nil
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
+		case "up", "k":
+			m.processTable.MoveUp(1)
+			return m, nil
+		case "down", "j":
+			m.processTable.MoveDown(1)
+			return m, nil
 		}
+
+	// Handle the TickMsg to update system stats
 	case TickMsg:
 		m.lastUpdate = time.Time(msg)
 
